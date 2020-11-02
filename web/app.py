d3cc4c868ab3fb5767efd6692cdeac37708f2aa3
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-11-02 14:46:23 -0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -9,7 +9,7 @@ def form():
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return "The URL /data is accessed directly. Try going to /form to submit form"
     if request.method == 'POST':
         form_data = request.form
         return render_template('data.html',form_data = form_data)
