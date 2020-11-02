5e3ed07159315b5b662d6ad5fadabdc0e8afa618
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-11-02 14:43:06 -0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,31 +1,18 @@
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-
-# App config.
-DEBUG = True
+from flask import Flask,render_template,request
+ 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    
-    @app.route("/", methods=['GET', 'POST'])
-    def hello():
-        form = ReusableForm(request.form)
-        
-        print form.errors
-        if request.method == 'POST':
-            name=request.form['name']
-            print name
-        
-        if form.validate():
-        # Save the comment here.
-            flash('Hello ' + name)
-        else:
-            flash('Error: All the form fields are required. ')
-        
-        return render_template('hello.html', form=form)
-
-if __name__ == "__main__":
-    app.run()
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
