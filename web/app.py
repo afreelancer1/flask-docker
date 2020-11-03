eb1522e92d90d0b4e3d773094ac1afee7d288868
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-11-03 10:56:24 -0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import random
 
 # App config.
 DEBUG = True
@@ -13,14 +13,46 @@ testest = '''<datalist id="browsers">
 <option value="7"></option>
 <option value="9"></option>
 </datalist>
-<form action="/">
-<h3 style="text-align: center;"><br /><span style="color: #808080;">You want a prime number between 2 and <input type="text" id="mx" name="mx"/> ending in <input list="browsers" type="text" id="last" name="last"/></span></h3>
-  <input type="submit" value="Submit">
+<form action="/prime">
+<h3 style="text-align: center;"><br /><span style="color: #808080;">You want a prime number between 2 and <input type="text" id="mx" name="mx" /> ending in <input list="browsers" type="text" id="last" name="last" /></span></h3>
+<h3 style="text-align: center;"><input type="submit" value="Submit" /></h3>
 </form>'''
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
     
+    @app.route("/prime", methods=['GET', 'POST'])
+    def get_prime(mx, last):
+        number = 1000
+        end = '7'
+        all_numbers = []
+
+        for i in range(2, number + 1):
+            all_numbers.append(i)
+
+        x = all_numbers[0]
+        while x < len(all_numbers):
+            for i in all_numbers:
+                if i == x or x > i or x > math.sqrt(number):
+                    continue
+                elif i % x == 0:
+                    all_numbers.remove(i)
+            x = all_numbers[(all_numbers.index(x)) + 1]
+
+        last_int = {}
+        ends_in = ['2', '3', '5', '7', '1', '9']
+        for i in all_numbers:
+            l = str(i)[-1]
+            if not l in last_int:
+                last_int[l] = [i]
+            else:
+                last_int[l].append(i)
+
+        prime = last_int[end][random.randint(0, len(last_int[end]))]
+
+
+        return "Your prime number is " + str(prime)
+
     @app.route("/", methods=['GET', 'POST'])
     def hello():
         form = ReusableForm(request.form)
