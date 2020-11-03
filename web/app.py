78bd49d4b9b47c745a282a1442e1e551586eb899
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-11-03 13:09:08 -0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -18,42 +18,39 @@ class primeForm(FlaskForm):
     submit = SubmitField('Submit')
 
     @app.route('/', methods=['GET', 'POST'])
-    def login():
-        form = primeForm()
-        if form.validate_on_submit():
-            flash('Prime requested upto {}'.format(
-                form.mx.data))
-            return redirect('/prime')
-        return render_template('prime.html', title='Your Prime', form=form)
+    def get_info():
+        if request.method == 'POST':  # this block is only entered when the form is submitted
+            number = request.form.get('mx')
+            end = request.form.get('last')
+            all_numbers = []
 
-    @app.route("/prime", methods=['GET', 'POST'])
-    def get_prime():
-        number = request.args.get('mx')
-        end = request.args.get('last')
-        all_numbers = []
+            for i in range(2, number + 1):
+                all_numbers.append(i)
 
-        for i in range(2, number + 1):
-            all_numbers.append(i)
+            x = all_numbers[0]
+            while x < len(all_numbers):
+                for i in all_numbers:
+                    if i == x or x > i or x > math.sqrt(number):
+                        continue
+                    elif i % x == 0:
+                        all_numbers.remove(i)
+                x = all_numbers[(all_numbers.index(x)) + 1]
 
-        x = all_numbers[0]
-        while x < len(all_numbers):
+            last_int = {}
             for i in all_numbers:
-                if i == x or x > i or x > math.sqrt(number):
-                    continue
-                elif i % x == 0:
-                    all_numbers.remove(i)
-            x = all_numbers[(all_numbers.index(x)) + 1]
+                l = str(i)[-1]
+                if not l in last_int:
+                    last_int[l] = [i]
+                else:
+                    last_int[l].append(i)
 
-        last_int = {}
-        for i in all_numbers:
-            l = str(i)[-1]
-            if not l in last_int:
-                last_int[l] = [i]
-            else:
-                last_int[l].append(i)
-
-        prime = last_int[end][random.randint(0, len(last_int[end]))]
-        return prime
+            prime = last_int[end][random.randint(0, len(last_int[end]))]
+            return prime
+        else:
+            return '''<form method="POST">
+    <p style="text-align: center;">You want a prime number between 2 and&nbsp; <input type="text" name="mx" /> that ends with&nbsp; <input type="text" name="last" /></p>
+    <p style="text-align: center;"><br /> <input type="submit" value="Submit" /></p>
+    </form>'''
 
 
 app.run(host='0.0.0.0', port=5000)
